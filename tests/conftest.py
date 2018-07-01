5a15945b56de194e39a09cbc20346176aadab5ca
Arcady Usov <itcrab@gmail.com>
2018-07-02 02:11:52 +0500

    Fix Travis-CI build - step 1

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,6 +74,6 @@ def repository_dict_third(tmpdir):
 
 @pytest.fixture
 def git_author():
-    user_name = subprocess.check_output('git config --global user.name', universal_newlines=True).strip()
-    user_email = subprocess.check_output('git config --global user.email', universal_newlines=True).strip()
+    user_name = subprocess.check_output('git config user.name', universal_newlines=True).strip()
+    user_email = subprocess.check_output('git config user.email', universal_newlines=True).strip()
     return '{} <{}>'.format(user_name, user_email)
