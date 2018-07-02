977699dd9343b0c884f680d3c5c12c56cab1ee0f
Arcady Usov <itcrab@gmail.com>
2018-07-02 17:41:07 +0500

    Refactoring Travis-CI fixes build

--- tests/test_contributions_graph.py
+++ tests/test_contributions_graph.py
@@ -10,9 +10,9 @@ from contributions_graph.repository_list import RepositoryList
 class TestCommitsStore:
     def test_commits_store_with_obfuscate(self, tmpdir, datetime_string_first, datetime_string_second, git_author):
         git_repo_path = tmpdir.mkdir('git_repo')
-        os.chdir(str(git_repo_path))
+        os.chdir(git_repo_path.strpath)
 
-        new_repo_path = str(git_repo_path)
+        new_repo_path = git_repo_path.strpath
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
         git.create_repository()
@@ -26,25 +26,26 @@ class TestCommitsStore:
         new_repo_path = tmpdir.mkdir('new_git_repo')
 
         repository_list = RepositoryList()
-        repository_list.add(str(git_repo_path), 'master', git_author)
+        repository_list.add(git_repo_path.strpath, 'master', git_author)
 
-        git = Git(str(new_repo_path), 'py')
+        git = Git(new_repo_path.strpath, 'py')
         obfuscate = Obfuscate(11, 0, 0, 5)
         commit_store = ContributionsGraph(repository_list, git, obfuscate)
         commit_store.run()
 
-        os.chdir(str(new_repo_path))
+        os.chdir(new_repo_path.strpath)
+
         cmd = 'git --no-pager log --pretty="%cI" --author="{}"'.format(git_author)
-        all_commits = subprocess.check_output(cmd, shell=True).decode('utf-8')
+        all_commits = subprocess.check_output(cmd, shell=True, universal_newlines=True)
         all_commits = all_commits.splitlines()
 
         assert all_commits == ['2018-06-30T11:10:00+05:00', '2018-06-30T11:05:00+05:00']
 
     def test_commits_store_without_obfuscate(self, tmpdir, datetime_string_first, datetime_string_second, git_author):
         git_repo_path = tmpdir.mkdir('git_repo')
-        os.chdir(str(git_repo_path))
+        os.chdir(git_repo_path.strpath)
 
-        new_repo_path = str(git_repo_path)
+        new_repo_path = git_repo_path.strpath
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
         git.create_repository()
@@ -58,15 +59,16 @@ class TestCommitsStore:
         new_repo_path = tmpdir.mkdir('new_git_repo')
 
         repository_list = RepositoryList()
-        repository_list.add(str(git_repo_path), 'master', git_author)
+        repository_list.add(git_repo_path.strpath, 'master', git_author)
 
-        git = Git(str(new_repo_path), 'py')
+        git = Git(new_repo_path.strpath, 'py')
         commit_store = ContributionsGraph(repository_list, git)
         commit_store.run()
 
-        os.chdir(str(new_repo_path))
+        os.chdir(new_repo_path.strpath)
+
         cmd = 'git --no-pager log --pretty="%cI" --author="{}"'.format(git_author)
-        all_commits = subprocess.check_output(cmd, shell=True).decode('utf-8')
+        all_commits = subprocess.check_output(cmd, shell=True, universal_newlines=True)
         all_commits = all_commits.splitlines()
 
         assert all_commits == ['2018-06-30T23:22:01+05:00', '2018-06-30T20:12:09+05:00']
--- tests/test_git.py
+++ tests/test_git.py
@@ -12,17 +12,17 @@ class TestGit:
     def test_new_repo_path_exists(self, tmpdir):
         tmpdir.mkdir('.git')
 
-        new_repo_path = str(tmpdir)
+        new_repo_path = tmpdir.strpath
         file_ext = 'py'
         with pytest.raises(GitRepositoryExistsError):
             Git(new_repo_path, file_ext)
 
     def test_get_commits(self, tmpdir, datetime_string_first, datetime_string_second, git_author):
         git_repo_path = tmpdir.mkdir('get_commits')
-        os.chdir(str(git_repo_path))
+        os.chdir(git_repo_path.strpath)
         os.system('git init')
 
-        new_repo_path = os.path.join(str(git_repo_path), 'sub_dir')
+        new_repo_path = os.path.join(git_repo_path.strpath, 'sub_dir')
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
 
@@ -33,7 +33,7 @@ class TestGit:
         git.commit_file(datetime_string_second, file_name)
 
         all_commits = git.get_commits(
-            repo_path=str(git_repo_path),
+            repo_path=git_repo_path.strpath,
             branch='master',
             author=git_author
         )
@@ -41,18 +41,18 @@ class TestGit:
         assert all_commits == [datetime_string_second, datetime_string_first]
 
     def test_create_repository(self, tmpdir):
-        new_repo_path = str(tmpdir)
+        new_repo_path = tmpdir.strpath
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
 
-        git_repo_path = os.path.join(str(new_repo_path), '.git')
+        git_repo_path = os.path.join(new_repo_path, '.git')
         assert os.path.isdir(git_repo_path) is False
 
         git.create_repository()
         assert os.path.isdir(git_repo_path) is True
 
     def test_create_repository_with_wrong_path(self, tmpdir):
-        new_repo_path = os.path.join(str(tmpdir), 'wrong')
+        new_repo_path = os.path.join(tmpdir.strpath, 'wrong')
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
 
@@ -64,21 +64,21 @@ class TestGit:
 
     def test_build_repository(self, tmpdir, datetime_string_first, datetime_string_second, git_author):
         git_repo_path = tmpdir.mkdir('builg_git_repository')
-        os.chdir(str(git_repo_path))
+        os.chdir(git_repo_path.strpath)
 
         all_commits = [
             parse_iso_8601_string_to_datetime(datetime_string_first),
             parse_iso_8601_string_to_datetime(datetime_string_second),
         ]
 
-        new_repo_path = str(git_repo_path)
+        new_repo_path = git_repo_path.strpath
         file_ext = 'py'
         git = Git(new_repo_path, file_ext)
         git.create_repository()
         git.build_repository(all_commits)
 
         cmd = 'git --no-pager log --pretty="%cI" --author="{}"'.format(git_author)
-        all_commits = subprocess.check_output(cmd, shell=True).decode('utf-8')
+        all_commits = subprocess.check_output(cmd, shell=True, universal_newlines=True)
         all_commits = all_commits.splitlines()
 
         assert all_commits == ['2018-06-30T23:22:01+05:00', '2018-06-30T20:12:09+05:00']
